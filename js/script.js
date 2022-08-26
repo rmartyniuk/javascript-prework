1b0e5d2ff19c8774398ff4d2567196a9dc903a3c
Robert Martyniuk <r.martyniuk1234@gmail.com>
2022-08-26 15:53:54 +0200

    add var rock and scissors

--- js/script.js
+++ js/script.js
@@ -1,23 +1,29 @@
 var computerMove;
 computerMove = 'papier';
+printMessage('Zagrałem' + computerMove + '! Jeśli Twój ruch to nożyce, to wygrywasz!');
+var computerMove;
+computerMove = 'kamień';
+printMessage('Zagrałem' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+var computerMove;
+computerMove = 'nożyce';
 printMessage('Zagrałem' + computerMove + '! Jeśli Twój ruch to kamień, to wygrywasz!');
 
 var playerMove, playerInput;
 playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 console.log('Wpisana odpowiedź to: ' + playerInput);
 if (playerInput == '1') {
-  playerMove = 'kamień';
-  printMessage('Twój ruch: ' + playerMove);
-} else {
-  if (playerInput == '2') {
-    playerMove = 'nożyce';
+    playerMove = 'kamień';
     printMessage('Twój ruch: ' + playerMove);
-  } else {
-    if (playerInput == '3') {
-      playerMove = 'papier';
-      printMessage('Twój ruch: ' + playerMove);
+} else {
+    if (playerInput == '2') {
+        playerMove = 'nożyce';
+        printMessage('Twój ruch: ' + playerMove);
     } else {
-      playerMove = 'nieznany ruch';
+        if (playerInput == '3') {
+            playerMove = 'papier';
+            printMessage('Twój ruch: ' + playerMove);
+        } else {
+            playerMove = 'nieznany ruch';
+        }
     }
-  }
 }
