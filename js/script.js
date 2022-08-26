b203928e3a9dab09b4fbddb4db00f0d06c42ca64
Robert Martyniuk <r.martyniuk1234@gmail.com>
2022-08-26 13:53:08 +0200

    add var in script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,25 @@
 var computerMove;
 computerMove = 'papier';
-printMessage('Zagrałem'
-computerMove + '! Jeśli Twój ruch to kamień, to wygrywasz!');)
+printMessage('Zagrałem' + computerMove + '! Jeśli Twój ruch to kamień, to wygrywasz!');
+
+var playerMove, playerInput;
+playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('Wpisana odpowiedź to: ' + playerInput);
+if (playerInput == '1') {
+    playerMove = 'kamień';
+} else {
+    playerMove = 'nieznany ruch';
+}
+printMessage('Twój ruch: ' + playerMove);
+if (playerInput == '2') {
+    playerMove = 'kamień';
+} else {
+    playerMove = 'nieznany ruch';
+}
+printMessage('Twój ruch: ' + playerMove);
+if (playerInput == '3') {
+    playerMove = 'kamień';
+} else {
+    playerMove = 'nieznany ruch';
+}
+printMessage('Twój ruch: ' + playerMove);
