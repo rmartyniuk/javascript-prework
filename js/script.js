3d2eb151cb8fbe5cce90a0d78ee1bc1493fbc783
Robert Martyniuk <r.martyniuk1234@gmail.com>
2022-08-27 09:30:36 +0200

    structure scritp.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 var computerMove;
-computerMove = 'papier';
-printMessage('Zagrałem' + computerMove + '! Jeśli Twój ruch to nożyce, to wygrywasz!');
-var computerMove;
 computerMove = 'kamień';
 printMessage('Zagrałem' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+var computerMove;
+computerMove = 'papier';
+printMessage('Zagrałem' + computerMove + '! Jeśli Twój ruch to nożyce, to wygrywasz!');
 var computerMove;
 computerMove = 'nożyce';
 printMessage('Zagrałem' + computerMove + '! Jeśli Twój ruch to kamień, to wygrywasz!');
@@ -15,10 +15,10 @@ if (playerInput == '1') {
     playerMove = 'kamień';
     printMessage('Twój ruch: ' + playerMove);
 } else if (playerInput == '2') {
-    playerMove = 'nożyce';
+    playerMove = 'papier';
     printMessage('Twój ruch: ' + playerMove);
 } else if (playerInput == '3') {
-    playerMove = 'papier';
+    playerMove = 'nożyce';
     printMessage('Twój ruch: ' + playerMove);
 } else {
     printMessage('nieznany ruch');
